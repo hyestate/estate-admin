62c17139e9fcaff98f79a6f590295bf78712ffa8
huangjilaiqin <1577594730@qq.com>
2018-11-25 20:49:03 +0800

    commit

--- config/config.default.js
+++ config/config.default.js
@@ -5,7 +5,8 @@ exports.keys = 'my secret keys';
 exports.sequelize = {
     dialect: 'mysql',
     database: 'estate',
-    host: 'localhost',
+    //host: 'localhost',
+    host: '47.98.153.117',
     port: '3306',
     username: 'estate',
     password: 'estate_passwd'
